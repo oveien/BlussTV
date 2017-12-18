766c7981fe295f107616b6d4fb84d9aa4546ff04
Thor Even Tutturen <thor@bustbyte.no>
2017-12-18 20:01:27 +0100

    fix substitution angular logic

--- app/components/playersubstitution/playersubstitution.js
+++ app/components/playersubstitution/playersubstitution.js
@@ -1,100 +1,107 @@
-(function (angular) {
-    var app = angular.module('blussTV');
-    app.controller('playerSubstitutionController', ['$scope', 'CasparCGService', 'GameService', function ($scope, CasparCGService, GameService) {
-        $scope.showing = false;
-        $scope.players = [];
-        $scope.teamShowing = false;
+(function(angular) {
+  var app = angular.module('blussTV');
+  app.controller('playerSubstitutionController', [
+    '$scope',
+    'CasparCGService',
+    'GameService',
+    function($scope, CasparCGService, GameService) {
+      $scope.showing = false;
+      $scope.players = [];
+      $scope.teamShowing = false;
+      $scope.currentPlayerIn = null;
+
+      $scope.homeTeam = GameService.getTeam('home');
+      $scope.awayTeam = GameService.getTeam('away');
+
+      $scope.toggleShowTeam = function(team) {
         $scope.currentPlayerIn = null;
+        $scope.currentPlayerOut = null;
 
-        $scope.homeTeam = GameService.getTeam('home');
-        $scope.awayTeam = GameService.getTeam('away');
+        $scope.team = team;
 
-        $scope.toggleShowTeam = function (team) {
+        var team = GameService.getTeam(team);
 
-            $scope.currentPlayerIn = $scope.currentPlayerOut = null;
-
-            var team = GameService.getTeam(team);
-
-            if (team.name == $scope.teamShowing.name) {
-                $scope.players = [];
-                $scope.teamShowing = false;
-                return;
-            }
-
-            $scope.teamShowing = team;
-            var players = [];
-            for (var i in $scope.teamShowing.players) {
-                if (!$scope.teamShowing.players[i].deleted) {
-                    players.push($scope.teamShowing.players[i])
-                }
-            }
-            $scope.players = players;
+        if (team.name == $scope.teamShowing.name) {
+          $scope.players = [];
+          $scope.teamShowing = false;
+          return;
         }
 
-        $scope.selectPlayer = function (player) {
+        $scope.teamShowing = team;
+        var players = [];
+        for (var i in $scope.teamShowing.players) {
+          if (!$scope.teamShowing.players[i].deleted) {
+            players.push($scope.teamShowing.players[i]);
+          }
+        }
+        $scope.players = players;
+      };
 
-            if (!$scope.currentPlayerIn) {
-                $scope.currentPlayerIn = player;
-                $scope.teamShowing = true;
+      $scope.selectPlayer = function(player) {
+        if (!$scope.currentPlayerIn) {
+          $scope.currentPlayerIn = player;
+          $scope.teamShowing = true;
 
-                var p = [];
-                for (var i in $scope.players) {
-                    if ($scope.players[i] == player) {
-                        continue;
-                    }
-                    p.push($scope.players[i]);
-                }
-                $scope.players = p;
+          var p = [];
+          for (var i in $scope.players) {
+            if ($scope.players[i] == player) {
+              continue;
             }
-            else {
-                $scope.currentPlayerOut = player;
-                $scope.teamShowing = false;
-                $scope.players = [];
-            }
-
-            $scope.currentPlayer = player;
-
-            $scope.currentPlayerIn.team = {
-                logo: $scope.teamShowing.logo,
-                name: $scope.teamShowing.name
-            };
-
+            p.push($scope.players[i]);
+          }
+          $scope.players = p;
+        } else {
+          $scope.currentPlayerOut = player;
+          $scope.teamShowing = false;
+          $scope.players = [];
         }
 
-        $scope.toggleShowPlayer = function () {
-            if (CasparCGService.getCurrentOverlay() == 'substitution') {
-                CasparCGService.removeOverlay();
-                $scope.showing = false;
-            }
-            else {
-                $scope.showing = 'substitution';
+        $scope.currentPlayer = player;
 
-                var data = {
-                    playerIn: $scope.currentPlayerIn,
-                    playerOut: $scope.currentPlayerOut,
-                    team: $scope.currentPlayerIn.team
-                }
+        $scope.currentPlayerIn.team = {
+          logo: $scope.teamShowing.logo,
+          name: $scope.teamShowing.name,
+        };
+      };
 
-                CasparCGService.runOverlay('substitution', data);
-            }
+      $scope.toggleShowPlayer = function() {
+        if (CasparCGService.getCurrentOverlay() == 'substitution') {
+          CasparCGService.removeOverlay();
+          $scope.showing = false;
+        } else {
+          $scope.showing = 'substitution';
+
+          var data = {
+            playerIn: $scope.currentPlayerIn,
+            playerOut: $scope.currentPlayerOut,
+            team: $scope.team == 'home' ? $scope.homeTeam : $scope.awayTeam,
+            homeTeam: $scope.homeTeam,
+            awayTeam: $scope.awayTeam,
+          };
+
+          CasparCGService.runOverlay('substitution', data);
         }
+      };
 
-        CasparCGService.registerObserverCallback(['overlay-play', 'overlay-remove'], function (type, data) {
+      CasparCGService.registerObserverCallback(
+        ['overlay-play', 'overlay-remove'],
+        function(type, data) {
+          if (type == 'overlay-remove') {
+            $scope.showing = false;
+            return;
+          }
 
-            if (type == 'overlay-remove') {
-                $scope.showing = false;
-                return;
-            }
-
-            // New overlay:
-            if (data.template != 'substitution' && data.template != 'substitution') {
-                $scope.showing = false;
-            }
-            else {
-                $scope.showing = data.template;
-            }
-
-        });
-
-    }]);
+          // New overlay:
+          if (
+            data.template != 'substitution' &&
+            data.template != 'substitution'
+          ) {
+            $scope.showing = false;
+          } else {
+            $scope.showing = data.template;
+          }
+        },
+      );
+    },
+  ]);
 })(window.angular);
--- app/templates/mizuno/substitution/script.js
+++ app/templates/mizuno/substitution/script.js
@@ -1,49 +1,54 @@
-function play(str) {
-    var data = JSON.parse(str).data;
-    document.getElementById('out-number').innerHTML = data.playerOut.number;
-    document.getElementById('out-name').innerHTML = data.playerOut.name;
-    document.getElementById('in-number').innerHTML = data.playerIn.number;
-    document.getElementById('in-name').innerHTML = data.playerIn.name;
-    document.getElementById('logo').src = data.team.logo;
+function shortName(fullName) {
+  const names = fullName.split(' ');
+  return names[names.length - 1];
+}
 
-    // handle animation
-    $('.top-description').velocity({ opacity: 1 }, { duration: 200 });
-    $('.sub-content').velocity({ height: 114 }, { duration: 200 });
-    $('.logo-container').velocity({ height: 114 }, { duration: 200 });
-    $('.fade-in').velocity({ opacity: 1 }, { duration: 300, delay: 300 });
-    $('.bottom-border').velocity({ opacity: 1 }, { duration: 300, delay: 300 });
+function play(str) {
+  var data = JSON.parse(str).data;
+  console.log('sub data', data);
+  document.getElementById('out-number').innerHTML = data.playerOut.number;
+  document.getElementById('out-name').innerHTML = shortName(
+    data.playerOut.name,
+  );
+  document.getElementById('in-number').innerHTML = data.playerIn.number;
+  document.getElementById('in-name').innerHTML = shortName(data.playerIn.name);
+  document.getElementById('logo').src = data.team.logo;
+
+  // handle animation
+  $('.top-description').velocity({ opacity: 1 }, { duration: 200 });
+  $('.sub-content').velocity({ height: 114 }, { duration: 200 });
+  $('.logo-container').velocity({ height: 114 }, { duration: 200 });
+  $('.fade-in').velocity({ opacity: 1 }, { duration: 300, delay: 300 });
+  $('.bottom-border').velocity({ opacity: 1 }, { duration: 300, delay: 300 });
 }
 
 function remove(str) {
-    $('.top-description').velocity(
-        { opacity: 0 },
-        { duration: 200, delay: 100 },
-    );
-    $('.sub-content').velocity({ height: 0 }, { duration: 200, delay: 100 });
-    $('.logo-container').velocity({ height: 0 }, { duration: 200, delay: 100 });
-    $('.fade-in').velocity({ opacity: 0 }, { duration: 100 });
-    $('.bottom-border').velocity({ opacity: 0 }, { duration: 100 });
+  $('.top-description').velocity({ opacity: 0 }, { duration: 200, delay: 100 });
+  $('.sub-content').velocity({ height: 0 }, { duration: 200, delay: 100 });
+  $('.logo-container').velocity({ height: 0 }, { duration: 200, delay: 100 });
+  $('.fade-in').velocity({ opacity: 0 }, { duration: 100 });
+  $('.bottom-border').velocity({ opacity: 0 }, { duration: 100 });
 }
 
 if (getUrlParameter('debug')) {
-    setTimeout(function() {
-        play(
-            JSON.stringify({
-                data: {
-                    playerIn: {
-                        name: 'Øystein Veien',
-                        number: 14,
-                    },
-                    playerOut: {
-                        name: 'Sindre Svendby',
-                        number: 8,
-                    },
-                    team: {
-                        name: 'TIF Viking',
-                        logo: '/graphics/logo/viking.svg',
-                    },
-                },
-            }),
-        );
-    }, 50);
+  setTimeout(function() {
+    play(
+      JSON.stringify({
+        data: {
+          playerIn: {
+            name: 'Øystein Veien',
+            number: 14,
+          },
+          playerOut: {
+            name: 'Sindre Svendby',
+            number: 8,
+          },
+          team: {
+            name: 'TIF Viking',
+            logo: '/graphics/logo/viking.svg',
+          },
+        },
+      }),
+    );
+  }, 50);
 }
