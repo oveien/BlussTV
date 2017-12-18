2e93a07d862ba94e058f51744da26e201eb9ea0e
Thor Even Tutturen <thor@bustbyte.no>
2017-12-18 22:09:06 +0100

    add teams playing

---
+++ app/templates/mizuno/teamsplaying/script.js
@@ -0,0 +1,30 @@
+function play(str) {
+  var data = JSON.parse(str).data;
+
+  document.getElementById('homeTeamLogo').src = data.homeTeam.logo;
+  document.getElementById('homeTeamName').innerHTML = data.homeTeam.name;
+
+  document.getElementById('awayTeamLogo').src = data.awayTeam.logo;
+  document.getElementById('awayTeamName').innerHTML = data.awayTeam.name;
+
+  $('.tp-container').velocity({ height: 100, opacity: 1 });
+  $('.fade-in').velocity({ opacity: 1 }, { delay: 300 });
+}
+
+function remove(str) {
+  $('.fade-in').velocity({ opacity: 0 });
+  $('.tp-container').velocity({ height: 0, opacity: 0 }, { delay: 300 });
+}
+
+if (getUrlParameter('debug')) {
+  setTimeout(function() {
+    play(
+      JSON.stringify({
+        data: {
+          homeTeam: { logo: '/graphics/logo/bktromso.svg', name: 'BK Tromsø' },
+          awayTeam: { logo: '/graphics/logo/viking.svg', name: 'TIF Viking' },
+        },
+      }),
+    );
+  }, 10);
+}
